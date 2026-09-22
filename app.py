4e53bef366c25ee448858243ac79cca945940740
Femipbiju <femipbiju@gmail.com>
2026-09-22 10:55:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 import streamlit as st
 import tensorflow as tf
 import numpy as np
+import pandas as pd
+
+# Load the trained model
 model = tf.keras.models.load_model("employee_performance_ann.keras")
+
 st.title("Employee Performance Prediction")
-st.write("Enter the employee's training hours and attendance to predict performance.")
+
+st.write(
+    "Enter the employee's training hours and attendance "
+    "to predict performance."
+)
+
+# Input fields
 study_hours = st.number_input(
     "Training Hours",
     min_value=0.0,
@@ -18,11 +28,16 @@ attendance = st.number_input(
     value=75.0
 )
 
+# Prediction
 if st.button("Predict"):
 
     input_data = np.array([[study_hours, attendance]])
 
-    probability = model.predict(input_data, verbose=0)[0][0]
+    probability = model.predict(
+        input_data,
+        verbose=0
+    )[0][0]
+
     prediction = 1 if probability >= 0.5 else 0
 
     if prediction == 1:
@@ -30,5 +45,6 @@ if st.button("Predict"):
     else:
         st.error("Needs Improvement")
 
-
-    st.write(f"Probability of Good Performance: {probability:.2%}")
+    st.write(
+        f"Probability of Good Performance: {probability:.2%}"
+    )
